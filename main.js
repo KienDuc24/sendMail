bf3673537de7e817431443e7094465438fca2cc4
KienDuc24 <92088660+KienDuc24@users.noreply.github.com>
2024-10-01 02:56:19 +0700

    up

---
+++ main.js
@@ -0,0 +1,33 @@
+document.getElementById('mail').addEventListener('keydown', handleKeyDown);
+document.getElementById('name').addEventListener('keydown', handleKeyDown);
+
+function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Ngăn chặn hành vi mặc định
+        document.getElementById('emailForm').dispatchEvent(new Event('submit'));
+    }
+}
+
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const mail = document.getElementById('mail').value;
+    const name = document.getElementById('name').value;
+
+    if (mail === "" || name === "") {
+        window.alert("Nhập đầy đủ đi nha.");
+    } else {
+        fetch('/api/confirm-email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ mail, name })
+        })
+        .then(response => {
+            if (!response.ok) throw new Error('Error sending email');
+            return response.text();
+        })
+        .then(data => alert(data))
+        .catch(error => alert('Có lỗi xảy ra: ' + error.message));
+    }
+});
